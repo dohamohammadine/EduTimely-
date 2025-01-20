0ee69729357870095df5a0a0549efb94b1d7f50d
dohamohammadine <mohammadinedoha@gmail.com>
2025-01-20 09:03:25 +0000

    Premier commit : ajout des fichiers du projet

---
+++ src/main/java/com/example/projet_collectif/ProjetCollectifApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_collectif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCollectifApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetCollectifApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_collectif/ProjetCollectifApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_collectif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCollectifApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
